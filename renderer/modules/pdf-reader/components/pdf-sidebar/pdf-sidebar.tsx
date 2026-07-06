4193c0e77eefb5b15d70d1bb73182dfbe4234f3c
Kanade <kanadesupreme@gmail.com>
2026-07-06 19:07:57 +0700

    feat: add button to go back the document list

--- renderer/modules/pdf-reader/components/pdf-sidebar/pdf-sidebar.tsx
+++ renderer/modules/pdf-reader/components/pdf-sidebar/pdf-sidebar.tsx
@@ -1,3 +1,6 @@
+import { IconButton } from "@renderer/modules/design-system";
+import { useRouter } from "@tanstack/react-router";
+import { ChevronLeft } from "lucide-react";
 import { PdfOutline } from "./pdf-outline";
 
 type PdfSidebarProps = {
@@ -5,10 +8,18 @@ type PdfSidebarProps = {
 };
 
 export function PdfSidebar({ documentId }: PdfSidebarProps) {
+	const router = useRouter();
 	return (
-		<aside className="flex h-full w-72 shrink-0 flex-col rounded-md bg-sidebar py-3 pl-3">
-			<h2 className="mb-2 px-1 font-medium text-sm">Outline</h2>
-			<PdfOutline documentId={documentId} />
-		</aside>
+		<div className="flex flex-col gap-2 h-full w-72 shrink-0">
+			<aside className="flex items-center gap-2 rounded-md bg-sidebar px-2 py-1">
+				<IconButton onClick={() => router.navigate({ to: "/" })}>
+					<ChevronLeft className="size-4" />
+				</IconButton>
+				<h2 className="font-medium">Outline</h2>
+			</aside>
+			<aside className="flex-1 rounded-md bg-sidebar py-3 pl-3">
+				<PdfOutline documentId={documentId} />
+			</aside>
+		</div>
 	);
 }
